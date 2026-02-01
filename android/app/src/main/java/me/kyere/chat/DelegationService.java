1b65d1d583f403aa7d57600fd7ea3e54fccbe754
cLLeB <168653764+cLLeB@users.noreply.github.com>
2026-02-01 21:04:37 +0000

    feat: setup TWA assets and enable FLAG_SECURE protection

---
+++ android/app/src/main/java/me/kyere/chat/DelegationService.java
@@ -0,0 +1,14 @@
+package me.kyere.chat;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
